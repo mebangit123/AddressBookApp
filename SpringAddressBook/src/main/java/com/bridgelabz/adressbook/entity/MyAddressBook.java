8abb2a070d38509408be4d9947cb8391752556b0
mebangit123 <mebannongrum333@gmail.com>
2021-08-15 12:00:20 +0530

    Created: Creating AddressBook App Spring project.

---
+++ SpringAddressBook/src/main/java/com/bridgelabz/adressbook/entity/MyAddressBook.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.adressbook.entity;
+
+public class MyAddressBook {
+	
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private int zip;
+	private int phoneNo;
+	private String email;	
+}
